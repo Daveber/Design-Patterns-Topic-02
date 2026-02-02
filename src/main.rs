8121d4d80eb147d90aeeedb9e3aedc0eb7f849f2
Dawid Beres <dawber12345@li-c3aadd4c-2e8f-11b2-a85c-8fa3a3c97d25.ibm.com>
2026-02-02 10:54:34 +0000

    02: Bitwise

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
-    let number: u8 = 0b0101_0101;
-    println!("Number is {number} and the type is {}", type_of(&number));
-    println!("Number as Binary {number:08b}");
+    let mut number: u8 = 0b0101_0101;
+
+    println!("Number is {number} binary is {number:08b}");
+
+    number = number | 0b0000_1111;
+    println!("bit 7 Number is {number} binary is {number:08b}");
 }
 
 fn type_of<T>(_: &T) -> &str {
