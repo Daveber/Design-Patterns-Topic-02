6675d90dc569bb5386616da7e5720bbe6bd7a190
Dawid Beres <dawber12345@li-c3aadd4c-2e8f-11b2-a85c-8fa3a3c97d25.ibm.com>
2026-02-02 10:47:09 +0000

    01: Binary completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let number: u8 = 0b0101_0101;
+    println!("Number is {number} and the type is {}", type_of(&number));
+    println!("Number as Binary {number:08b}");
+}
+
+fn type_of<T>(_: &T) -> &str {
+        return std::any::type_name::<T>()
 }
