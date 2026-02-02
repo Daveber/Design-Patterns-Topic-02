5cfd92a14de009c13439fdd961d7403692898bb1
Dawid Beres <dawber12345@li-c3aadd4c-2e8f-11b2-a85c-8fa3a3c97d25.ibm.com>
2026-02-02 11:01:54 +0000

    03: Bitwise

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ fn main() {
 
     println!("Number is {number} binary is {number:08b}");
 
-    number = number | 0b0000_1111;
-    println!("bit 7 Number is {number} binary is {number:08b}");
+    let number_left4 = number << 4;
+    println!("Number is {number_left4} \t binary is {number_left4:08b} when shifted left 4");
+
+    let number_right4 = number >> 4;
+    println!("Number is {number_right4} \t Binary is {number_right4:08b} when shifted right 4");
 }
 
 fn type_of<T>(_: &T) -> &str {
